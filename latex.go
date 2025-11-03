5203e6fd9fad4598e8f29106e5f371eb18151204
Sachin Singh <sachinsingh@digitalocean.com>
2025-11-03 20:18:22 +0530

    Add join and stability

--- latex.go
+++ latex.go
@@ -6,98 +6,7 @@ import (
 	"text/template"
 )
 
-var tikzTemplate = `\documentclass[border=10pt]{standalone}
-\usepackage{tikz}
-\usepackage{booktabs}
-\usepackage{array}
-\usetikzlibrary{arrows.meta, positioning}
-\definecolor{nodecolor}{RGB}{59, 130, 246}
-\definecolor{fingercolor}{RGB}{168, 85, 247}
-
-\begin{document}
-\begin{tikzpicture}[>=Stealth, line cap=round, line join=round]
-  
-  % Draw the ring
-  \def\R{3.5}
-  \draw[thick, gray!50] (0,0) circle (\R);
-  
-  % Ring size annotation
-  \node[gray, font=\footnotesize] at (0, -\R-0.5) {Ring Size: {{.RingSize}}};
-  
-  % Label circle with IDs
-  {{range $id := .IDLabels -}}
-  \node[gray, font=\tiny] at ({{$id.Angle}}:3.2) { {{- $id.ID -}} };
-  {{end}}
-  
-  % Draw nodes on the circle
-  {{range $idx, $node := .Nodes -}}
-  \fill[nodecolor] ({{$node.Angle}}:3.5) circle (3pt);
-  \node[nodecolor, font=\bfseries\small, anchor={{$node.Anchor}}] at ({{$node.Angle}}:3.8) { {{- $node.Label -}} };
-  \node[gray, font=\tiny, anchor={{$node.AnchorID}}] at ({{$node.Angle}}:4.1) {({{$node.ID}})};
-  {{end}}
-  
-  % Draw finger table arrows
-  {{range $arrow := .Arrows -}}
-  \draw[->, fingercolor!60, thick, shorten >=2pt] ({{$arrow.From}}:3.5) 
-    to[bend {{$arrow.Bend}}] ({{$arrow.To}}:3.5);
-  {{end}}
-  
-  % Finger tables positioned around the circle
-  {{range $idx, $table := .Tables -}}
-  \node[anchor={{$table.Anchor}}, font=\small] at ({{$table.X}}, {{$table.Y}}) {
-    \begin{tabular}{|c|c|c|}
-    \hline
-    \multicolumn{3}{|c|}{\textbf{ {{- $table.NodeLabel -}} (ID: {{$table.NodeID}})}} \\
-    \hline
-    \textbf{i} & \textbf{start} & \textbf{finger} \\
-    \hline
-    {{range $finger := $table.Fingers -}}
-    {{$finger.Index}} & {{$finger.Start}} & {{$finger.FingerID}} \\
-    {{end -}}
-    \hline
-    \end{tabular}
-  };
-  {{end}}
-  
-\end{tikzpicture}
-\end{document}
-`
-
-type NodeData struct {
-	Label    string
-	ID       uint32
-	Angle    float64
-	Anchor   string
-	AnchorID string
-}
-
-type ArrowData struct {
-	From string
-	To   string
-	Bend string
-}
-
-type FingerData struct {
-	Index    int
-	Start    uint32
-	FingerID uint32
-}
-
-type TableData struct {
-	NodeLabel string
-	NodeID    uint32
-	Fingers   []FingerData
-	X         float64
-	Y         float64
-	Anchor    string
-}
-
-type IDLabel struct {
-	ID    uint32
-	Angle float64
-}
-
-func GenerateChordVisualization(ring *Ring) error {
+func GenerateChordVisualization(nodes []*Node) error {
 	ringSize := uint32(1 << m)
 
 	var nodeData []NodeData
@@ -115,8 +24,8 @@ func GenerateChordVisualization(ring *Ring) error {
 	}
 
 	// Create node data for visualization
-	for i := 0; i < len(ring.nodes); i++ {
-		node := ring.nodes[i]
+	for i := 0; i < len(nodes); i++ {
+		node := nodes[i]
 		angle := (float64(node.id) / float64(ringSize)) * 360.0
 
 		// Determine anchor based on angle
@@ -222,7 +131,7 @@ func GenerateChordVisualization(ring *Ring) error {
 
 	fmt.Println("LaTeX file generated: chord_visualization.tex")
 	fmt.Println("\nNode Information:")
-	for _, node := range ring.nodes {
+	for _, node := range nodes {
 		fmt.Printf("\nNode: %s (ID: %d)\n", node.address, node.id)
 		fmt.Println("Finger Table:")
 		for i := 0; i < m; i++ {
@@ -233,3 +142,94 @@ func GenerateChordVisualization(ring *Ring) error {
 
 	return nil
 }
+
+type NodeData struct {
+	Label    string
+	ID       uint32
+	Angle    float64
+	Anchor   string
+	AnchorID string
+}
+
+type ArrowData struct {
+	From string
+	To   string
+	Bend string
+}
+
+type FingerData struct {
+	Index    int
+	Start    uint32
+	FingerID uint32
+}
+
+type TableData struct {
+	NodeLabel string
+	NodeID    uint32
+	Fingers   []FingerData
+	X         float64
+	Y         float64
+	Anchor    string
+}
+
+type IDLabel struct {
+	ID    uint32
+	Angle float64
+}
+
+var tikzTemplate = `\documentclass[border=10pt]{standalone}
+\usepackage{tikz}
+\usepackage{booktabs}
+\usepackage{array}
+\usetikzlibrary{arrows.meta, positioning}
+\definecolor{nodecolor}{RGB}{59, 130, 246}
+\definecolor{fingercolor}{RGB}{168, 85, 247}
+
+\begin{document}
+\begin{tikzpicture}[>=Stealth, line cap=round, line join=round]
+  
+  % Draw the ring
+  \def\R{3.5}
+  \draw[thick, gray!50] (0,0) circle (\R);
+  
+  % Ring size annotation
+  \node[gray, font=\footnotesize] at (0, -\R-0.5) {Ring Size: {{.RingSize}}};
+  
+  % Label circle with IDs
+  {{range $id := .IDLabels -}}
+  \node[gray, font=\tiny] at ({{$id.Angle}}:3.2) { {{- $id.ID -}} };
+  {{end}}
+  
+  % Draw nodes on the circle
+  {{range $idx, $node := .Nodes -}}
+  \fill[nodecolor] ({{$node.Angle}}:3.5) circle (3pt);
+  \node[nodecolor, font=\bfseries\small, anchor={{$node.Anchor}}] at ({{$node.Angle}}:3.8) { {{- $node.Label -}} };
+  \node[gray, font=\tiny, anchor={{$node.AnchorID}}] at ({{$node.Angle}}:4.1) {({{$node.ID}})};
+  {{end}}
+  
+  % Draw finger table arrows
+  {{range $arrow := .Arrows -}}
+  \draw[->, fingercolor!60, thick, shorten >=2pt] ({{$arrow.From}}:3.5) 
+    to[bend {{$arrow.Bend}}] ({{$arrow.To}}:3.5);
+  {{end}}
+  
+  % Finger tables positioned around the circle
+  {{range $idx, $table := .Tables -}}
+  \node[anchor={{$table.Anchor}}, font=\small] at ({{$table.X}}, {{$table.Y}}) {
+    \begin{tabular}{|c|c|c|}
+    \hline
+    \multicolumn{3}{|c|}{\textbf{ {{- $table.NodeLabel -}} (ID: {{$table.NodeID}})}} \\
+    \hline
+    \textbf{i} & \textbf{start} & \textbf{finger} \\
+    \hline
+    {{range $finger := $table.Fingers -}}
+    {{$finger.Index}} & {{$finger.Start}} & {{$finger.FingerID}} \\
+    {{end -}}
+    \hline
+    \end{tabular}
+  };
+  {{end}}
+  
+\end{tikzpicture}
+\end{document}
+`
--- main.go
+++ main.go
@@ -4,17 +4,18 @@ import (
 	"crypto/md5"
 	"encoding/binary"
 	"fmt"
+	"sync"
 )
 
 const m = 3
 
-func hashItem(item string, numNodes int) uint32 {
+func hashItem(item string) uint32 {
 	digest := md5.Sum([]byte(item))
 	digest1 := binary.BigEndian.Uint32(digest[0:4])
 	digest2 := binary.BigEndian.Uint32(digest[4:8])
 	digest3 := binary.BigEndian.Uint32(digest[8:12])
 	digest4 := binary.BigEndian.Uint32(digest[12:16])
-	return (digest1 ^ digest2 ^ digest3 ^ digest4) % (1 << numNodes)
+	return (digest1 ^ digest2 ^ digest3 ^ digest4) % (1 << m)
 }
 
 type Node struct {
@@ -23,84 +24,135 @@ type Node struct {
 	successor   *Node
 	predecessor *Node
 	finger      []*Node
+	keys        map[string]string
+	mutex       sync.RWMutex
 }
 
-func NewNode(address string, numNodes int) *Node {
+func NewNode(address string) *Node {
 	node := &Node{
-		id:      hashItem(address, numNodes),
+		id:      hashItem(address),
 		address: address,
 		finger:  make([]*Node, m),
+		keys:    make(map[string]string),
 	}
-
 	node.successor = node
 	node.predecessor = node
-
-	// finger table
-	for i := 0; i < m; i++ {
-		node.finger[i] = &Node{
-			id: (node.id + (1 << i)) % (1 << m),
-		}
+	for i := range node.finger {
+		node.finger[i] = node
 	}
-
 	return node
 }
 
 func (n *Node) findSuccessor(id uint32) *Node {
-	return n.findPredecessor(id).successor
+	n.mutex.RLock()
+	defer n.mutex.RUnlock()
+	pred := n.findPredecessor(id)
+	return pred.successor
 }
 
 func (n *Node) findPredecessor(id uint32) *Node {
+	n.mutex.RLock()
+	defer n.mutex.RUnlock()
+	n_prime := n
 	for {
-		if id >= n.id && id < n.successor.id {
-			return n
-		} else {
-			return n.closestPrecedingFinger(id)
+		if id > n_prime.id && id <= n_prime.successor.id ||
+			(n_prime.id >= n_prime.successor.id && (id > n_prime.id || id <= n_prime.successor.id)) {
+			break
 		}
+		cpf := n_prime.closestPrecedingFinger(id)
+		if cpf == n_prime {
+			break
+		}
+		n_prime = cpf
 	}
-
-	return n
+	return n_prime
 }
 
 func (n *Node) closestPrecedingFinger(id uint32) *Node {
-	for i := 0; i < m; i++ {
-		if n.finger[i].id > n.id && n.finger[i].id < id {
+	n.mutex.RLock()
+	defer n.mutex.RUnlock()
+	for i := m - 1; i >= 0; i-- {
+		if n.finger[i].id > n.id && n.finger[i].id < id ||
+			(n.id >= id && n.finger[i].id > n.id) {
 			return n.finger[i]
 		}
 	}
 	return n
 }
 
-// /////////////////////
-type Ring struct {
-	nodes      []*Node
-	nodeHashes []uint32
-}
-
-func NewRing(nodes []string) *Ring {
-	numNodes := len(nodes)
-	ring := &Ring{}
-
-	for i := 0; i < numNodes; i++ {
-		ring.nodes = append(ring.nodes, NewNode(nodes[i], numNodes))
+func (n *Node) join(n_prime *Node) {
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
+	if n_prime == nil {
+		return
 	}
-
-	return ring
-}
-
-///////////////////////
-
-func main() {
-	nodes := []string{"lubna", "lubbu", "booboo"}
-	ring := NewRing(nodes)
-	for i := 0; i < len(nodes); i++ {
-		fmt.Println("Node -> ", ring.nodes[i].id)
-		fmt.Println("Succesor -> ", ring.nodes[i].findSuccessor(1).id)
-		for _, finger := range ring.nodes[i].finger {
-			fmt.Println("- ", finger.id)
+	n.successor = n_prime.findSuccessor(n.id)
+	n.predecessor = n.successor.predecessor
+	n.successor.predecessor = n
+	if n.predecessor != nil {
+		n.predecessor.successor = n
+	}
+	for key, value := range n.successor.keys {
+		if hashItem(key) <= n.id {
+			n.keys[key] = value
+			delete(n.successor.keys, key)
 		}
 	}
+	n.buildFingerTable()
+}
 
-	//if err := GenerateChordVisualization(ring); err != nil {
-	//	fmt.Printf("Error generating visualization: %v\n", err)
-	//}
+func (n *Node) stabilize() {
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
+	x := n.successor.predecessor
+	if x != nil && (x.id > n.id && x.id < n.successor.id ||
+		n.successor.id <= n.id && (x.id > n.id || x.id <= n.successor.id)) {
+		n.successor = x
+	}
+	n.successor.notify(n)
+	n.fixFingers()
+}
+
+func (n *Node) notify(n_prime *Node) {
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
+	if n.predecessor == nil || (n_prime.id > n.predecessor.id && n_prime.id < n.id) {
+		n.predecessor = n_prime
+	}
+}
+
+func (n *Node) buildFingerTable() {
+	for i := 0; i < m; i++ {
+		start := (n.id + (1 << i)) % (1 << m)
+		n.finger[i] = n.findSuccessor(start)
+	}
+}
+
+func (n *Node) fixFingers() {
+	next := 0
+	start := (n.id + (1 << next)) % (1 << m)
+	n.finger[next] = n.findSuccessor(start)
+	next = (next + 1) % m
+}
+
+func main() {
+	node1 := NewNode("addr1")
+	node2 := NewNode("addr2")
+	node3 := NewNode("addr3")
+
+	node2.join(node1)
+	node3.join(node1)
+
+	node1.stabilize()
+	node2.stabilize()
+	node3.stabilize()
+
+	fmt.Printf("Node1 successor: %d\n", node1.successor.id)
+	fmt.Printf("Node2 successor: %d\n", node2.successor.id)
+
+	// Generate visualization
+	nodes := []*Node{node1, node2, node3}
+	if err := GenerateChordVisualization(nodes); err != nil {
+		fmt.Printf("Error generating visualization: %v\n", err)
+	}
 }
